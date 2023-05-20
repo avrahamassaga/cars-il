4c3d183c0f6b9658fbbce16beae906cdbf52cd34
avraham assaga <assagaavi@gmail.com>
2023-05-20 10:57:50 +0300

    fix date format

--- src/pages/components/showData/DisplayInfo.jsx
+++ src/pages/components/showData/DisplayInfo.jsx
@@ -31,24 +31,33 @@ export default function DisplayInfo(props) {
   const setInfo = useMemo(
     () =>
       Object.entries(car)
-        .filter(([key]) => cardata[key]) // Filter keys based on the presence of corresponding key in cardata object
+        .filter(([key, value]) => cardata[key]) // Filter keys based on the presence of corresponding key in cardata object
         .sort(([key1], [key2]) => keyOrder.indexOf(key1) - keyOrder.indexOf(key2)) // Sort keys according to the order in keyOrder array
-        .map(([key, value]) => (
-          <div key={key} className="flex flex-col-reverse w-full">
-            <div className="bg-[#2c6460] text-[#ffffff] text-[32px] font-bold drop-shadow-lg">
-              <p>
-                {value !== undefined && value !== null
-                  ? value.toString()
-                  : "לא צויין"}
-              </p>
+        .map(([key, value]) => {
+          // Check if the value is a date string and convert it to the desired format
+          if (typeof value === "string" && /^\d{4}-\d{2}-\d{2}$/.test(value)) {
+            value = new Date(value).toLocaleDateString("en-GB");
+          }
+  
+          return (
+            <div key={key} className="flex flex-col-reverse w-full">
+              <div className="bg-[#2c6460] text-[#ffffff] text-[32px] font-bold drop-shadow-lg">
+                <p>
+                  {value !== undefined && value !== null
+                    ? value.toString()
+                    : "לא צויין"}
+                </p>
+              </div>
+              <div className="bg-[#e0e0e0] text-[#172436] font-bold p-2 rounded-t-xl drop-shadow-lg w-full text-[22px]">
+                <p>{cardata[key] || key}</p>
+              </div>
             </div>
-            <div className="bg-[#e0e0e0] text-[#172436] font-bold p-2 rounded-t-xl drop-shadow-lg w-full text-[22px]">
-              <p>{cardata[key] || key}</p>
-            </div>
-          </div>
-        )),
+          );
+        }),
     [car, cardata]
   );
+  
+  
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
